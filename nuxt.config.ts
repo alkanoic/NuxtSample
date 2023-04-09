2f3f0a52cf010f031ad10c028c645fbeedb73390
alkanoic <naotoinaguma@hotmail.com>
2023-04-09 01:57:42 +0000

    add eslint settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  nitro: {
+    preset: "node",
+  },
+  srcDir: "src", // componentsやpagesなどのディレクトリを置く場所を指定
+  css: ["@/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
